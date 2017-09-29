c344c06b1362d4d4010d42cdb0bc4a5ddb3eb56c
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-09-29 12:48:08 -0700

    expand templates

---
+++ src/Microsoft.Extensions.HealthChecks.Abstractions/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microsoft.Extensions.HealthChecks.Abstractions
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Microsoft.Extensions.HealthChecks/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microsoft.Extensions.HealthChecks
+{
+    public class Class1
+    {
+    }
+}
